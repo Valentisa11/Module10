26610323d6505dc8cebc6fb6b83c1df1c225d44a
Valentisa11 <valya.kobylina@yandex.ru>
2022-12-27 19:22:14 +0500

    Добавьте файлы проекта.

---
+++ Calculate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    public class Calculate : ISum
+    {
+        ILogger Logger { get; }
+        public Calculate(ILogger logger)
+        {
+            Logger = logger;
+        }
+        int ISum.Sum(int a, int b)
+        {
+            Logger.Event("Ваш результат");//событие синим цветом
+            return a + b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Module10;
+using System;
+using System.IO;
+
+
+namespace Module10Task1_2
+{
+    class Program
+    {
+        static ILogger? Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            Calculate calculate = new(Logger);
+            int a, b;
+
+            Exception exceptionnum = new("Вы не ввели число");// ошибки, когда не введено число
+
+            
+            while (true)
+
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                
+                Console.WriteLine("\tКалькулятор\r");
+                Console.WriteLine("------------------------\n");
+
+                try
+                {
+
+                    Console.WriteLine("Введите число, а затем нажмите Enter");
+                    a = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число, а затем нажмите Enter");
+                    b = Convert.ToInt32(Console.ReadLine());
+
+                    var result = ((ISum)calculate).Sum(a, b);//выделение синим цветом
+                    Console.WriteLine(result);
+                    Console.ReadKey();
+                    
+                }
+
+
+
+
+                catch (Exception ex)
+                {
+
+                    Logger.Error("Вы не ввели число");
+                    Console.WriteLine(ex.Message);
+                    
+
+                }
+                
+                Console.ReadKey();
+                
+
+            } return;
+
+
+
+        }
+    }
+}
